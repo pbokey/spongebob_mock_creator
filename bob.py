26200a79cf781ed9ae55684f67e967aeee6ab5f1
Pranav Bokey <pbokey@gmail.com>
2017-06-18 22:08:43 -0400

    updated script

--- bob.py
+++ bob.py
@@ -1,13 +1,27 @@
 from random import randint
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import datetime
 
 inp = input('Enter text: ')
 def transform(strt):
     splitter = list(strt)
+    suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
     for i in range(len(splitter)):
         x = randint(0,1)
         if (x == 1):
             if (not splitter[i].isupper()):
                 splitter[i] = splitter[i].upper()
     x = "".join(splitter)
-    return x
-print(transform(inp))
+    img = Image.open("spongebob/img/bob.jpg")
+    font = ImageFont.truetype("spongebob/fonts/HelveticaNeueLt.ttf", 20)
+    draw = ImageDraw.Draw(img)
+    draw.text((20,10), x, (0,0,0), font=font)
+    draw = ImageDraw.Draw(img)
+    file_name = 'spongebob/out/' + strt + "_" + suffix + ".jpg"
+    img.save(file_name)
+    img_new = Image.open(file_name)
+    img.show()
+
+transform(inp)
